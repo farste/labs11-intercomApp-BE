e99474a99b4686cefcbb6192be0bdb8adf113b02
farste <sfargali@gmail.com>
2019-04-02 18:53:02 -0400

    first shot at adding NTS token

--- api/voice/voiceHandler.js
+++ api/voice/voiceHandler.js
@@ -2,15 +2,18 @@ require('dotenv');
 const ClientCapability = require('twilio').jwt.ClientCapability;
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
 const AccessToken = require('twilio').jwt.AccessToken;
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
 const VoiceGrant = AccessToken.VoiceGrant;
+const appSid = process.env.APP_SID;
+const pushCredSid = process.env.PUSH_CREDENTIAL_SID;
+const outgoingApplicationSid = process.env.APP_SID;
+
+
 
 exports.tokenGenerator = function tokenGenerator() {
   const identity = 'mee';
-  const accountSid = process.env.ACCOUNT_SID;
-  const authToken = process.env.AUTH_TOKEN;
-  const appSid = process.env.APP_SID;
-  const pushCredSid = process.env.PUSH_CREDENTIAL_SID;
-  const outgoingApplicationSid = process.env.APP_SID;
   const capability = new ClientCapability({
     accountSid: accountSid,
     authToken: authToken,
@@ -27,6 +30,8 @@ exports.tokenGenerator = function tokenGenerator() {
     grant: voiceGrant,
   }));
 
+  client.tokens.create().then(token => console.log(token.username));
+  
   // Include identity and token in a JSON response
   return {
     identity: identity,
