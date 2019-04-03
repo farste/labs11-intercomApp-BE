407b03373be05e547367c19c6ab18cdadef137b0
farste <sfargali@gmail.com>
2019-04-03 15:32:03 -0400

    refactored token to include voiceGrant

--- api/voice/voiceHandler.js
+++ api/voice/voiceHandler.js
@@ -7,9 +7,10 @@ const authToken = process.env.AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
 const VoiceGrant = AccessToken.VoiceGrant;
 const appSid = process.env.APP_SID;
+const apiKey = process.env.API_KEY;
+const apiKeySecret = process.env.API_KEY_SECRET;
 const pushCredSid = process.env.PUSH_CREDENTIAL_SID;
 const outgoingApplicationSid = process.env.APP_SID;
-const defaultIdentity = 'hohum';
 
 exports.generateRTCToken = function generateRTCToken() {
   client.tokens
@@ -19,66 +20,29 @@ exports.generateRTCToken = function generateRTCToken() {
 };
 
 exports.tokenGenerator = function tokenGenerator() {
-  function tokenGenerator(request, response) {
-    // Parse the identity from the http request
-    var identity = null;
-    if (request.method == 'POST') {
-      identity = request.body.identity;
-    } else {
-      identity = request.query.identity;
-    }
-  
-    if(!identity) {
-      identity = defaultIdentity;
-    }
-  
-    // Used when generating any kind of tokens
-    const accountSid = process.env.ACCOUNT_SID;
-    const apiKey = process.env.API_KEY;
-    const apiSecret = process.env.API_KEY_SECRET;
-  
-    // Used specifically for creating Voice tokens
-    const pushCredSid = process.env.PUSH_CREDENTIAL_SID;
-    const outgoingApplicationSid = process.env.APP_SID;
-  
-    // Create an access token which we will sign and return to the client,
-    // containing the grant we just created
-    const voiceGrant = new VoiceGrant({
-        outgoingApplicationSid: outgoingApplicationSid,
-        pushCredentialSid: pushCredSid
-      });
-  
-    // Create an access token which we will sign and return to the client,
-    // containing the grant we just created
-    const token = new AccessToken(accountSid, apiKey, apiSecret);
-    token.addGrant(voiceGrant);
-    token.identity = identity;
-    console.log('Token:' + token.toJwt());
-    return response.send(token.toJwt());
-  }
-/*   const identity = "mee";
-  const capability = new ClientCapability({
+  const identity = "mee";
+/*   const capability = new ClientCapability({
     accountSid: accountSid,
     authToken: authToken
-  });
+  });*/
   const voiceGrant = new VoiceGrant({
     outgoingApplicationSid: outgoingApplicationSid,
     pushCredentialSid: pushCredSid
-  });
+  }); 
 
-  capability.addScope(new ClientCapability.IncomingClientScope(identity));
+/*   capability.addScope(new ClientCapability.IncomingClientScope(identity));
   capability.addScope(
     new ClientCapability.OutgoingClientScope({
       applicationSid: appSid,
       clientName: identity,
-      grant: voiceGrant
     })
-  );
+  ); */
+  const token = new AccessToken(accountSid, apiKey, apiKeySecret);
+  token.addGrant(voiceGrant);
   // Include identity and token in a JSON response
   return {
-    identity: identity,
-    token: capability.toJwt(),
-  } */
+    token: token.toJwt(),
+  }
 };
 
 exports.voiceResponse = function voiceResponse(toNumber) {
